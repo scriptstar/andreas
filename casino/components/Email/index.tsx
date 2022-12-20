4ed12b2a30a42428ea8f458d1effbb065a7aa865
Narendra <scriptstar@users.noreply.github.com>
2022-12-20 15:01:01 +0000

    initial commit

---
+++ casino/components/Email/index.tsx
@@ -0,0 +1,6 @@
+import styles from "Email.module.css";
+function Email() {
+    return <div>Email</div>;
+}
+
+export default Email;
---
+++ casino/components/Title/index.tsx
@@ -0,0 +1,7 @@
+import styles from "Title.module.css";
+
+function Title() {
+    return <div>Title</div>;
+}
+
+export default Title;
